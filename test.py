c7cb08e2b0be039a5fe80618778afcbc6d7df730
Pedro Jose Lumbreras <pjlumbreras@gmail.com>
2020-09-15 21:29:22 +0200

    cambio por chat
    desde le chat

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+import pytest
+
 def inc(x):
-return x + 1
+    return x + 1
+
 class TestClass:
-def test_inc(self):
-x = 1
-assert inc(x) == 2
-def test_inc2(self):
-x = 2
-assert inc(x) == 2
+    def test_inc(self):
+        x = 1
+        assert inc(x) == 2
+
+    def test_inc2(self):
+        x = 2
+        assert inc(x) == 2
